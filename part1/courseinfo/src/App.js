6e39835249664b55eedb2bb87ca153aade2ea09f
Toni Patsias <enchanted.toilet@gmail.com>
2023-03-01 15:36:42 +0000

    Finished 1.5

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -18,39 +18,47 @@ const Content = (props) => {
   return (
     <div>
       <Part pizza={props.salad[0]} />
-      <Part pizza={props.salad[1]}/>
-      <Part pizza={props.salad[2]}/>
+      <Part pizza={props.salad[1]} />
+      <Part pizza={props.salad[2]} />
     </div>
   );
 };
 
 const Total = (props) => {
-  return <p>Number of exercises {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}</p>;
+  return (
+    <p>
+      Number of exercises{" "}
+      {props.parts[0].exercises +
+        props.parts[1].exercises +
+        props.parts[2].exercises}
+    </p>
+  );
 };
 
 const App = () => {
-  const course = "Half Stack application development";
-  const parts = [
-    {
-      name: "Fundamentals of React",
-      exercises: 10,
-    },
-    {
-      name: "Using props to pass data",
-      exercises: 7,
-    },
-    {
-      name: "State of a component",
-      exercises: 14,
-    },
-  ];
+  const course = {
+    name: "Half Stack application development",
+    parts: [
+      {
+        name: "Fundamentals of React",
+        exercises: 10,
+      },
+      {
+        name: "Using props to pass data",
+        exercises: 7,
+      },
+      {
+        name: "State of a component",
+        exercises: 14,
+      },
+    ],
+  };
 
   return (
     <div>
-      <Header course={course} />
-      <Content salad={parts} />
-      <Total parts={parts}
-      />
+      <Header course={course.name} />
+      <Content salad={course.parts} />
+      <Total parts={course.parts} />
     </div>
   );
 };
