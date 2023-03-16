473b194dd296c48ce34e8a40956d852f27f3da4f
Toni Patsias <enchanted.toilet@gmail.com>
2023-03-16 15:31:17 +0000

    Did 2.11: The Phonebook Step6

--- part2/thephonebook/src/App.js
+++ part2/thephonebook/src/App.js
@@ -1,31 +1,44 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
 import PersonForm from "./components/PersonForm";
 import Persons from "./components/Persons";
 import Filter from "./components/Filter";
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ]);
+  const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
-  const [selected, setSelected] = useState([]);
+  const [selected, setSelected] = useState(persons);
   const [newSearch, setNewSearch] = useState("");
+
+  const hook = () => {
+    console.log("effect")
+    axios
+      .get("http://localhost:3001/persons")
+      .then(response => {
+        console.log("promise fulfilled")
+        setPersons(response.data)
+        setSelected(response.data)
+      })
+  }
+
+  useEffect(hook, [])
 
   const addName = (event) => {
     event.preventDefault();
-    const nameObject = {
-      name: newName,
-      number: newNumber,
-      id: persons.length + 1,
-    };
 
     if (doesNameExist()) {
       alert(`${newName} is already added to the phonebook`);
-    } else setPersons(persons.concat(nameObject));
+    } else {
+      const nameObject = {
+        name: newName,
+        number: newNumber,
+        id: persons.length + 1,
+      };
+      setPersons(persons.concat(nameObject));
+      setSelected(persons.concat(nameObject));
+    }
+
     setNewName("");
     setNewNumber("");
   };
@@ -47,9 +60,11 @@ const App = () => {
      setSelected(entriesToShow);
   }
 
+
+
   const doesNameExist = () => {
     for (let i = 0; i < persons.length; i++) {
-      if (newName === persons[i].name) {
+      if (newName.toLowerCase() === persons[i].name.toLowerCase()) {
         return true;
       }
     }
