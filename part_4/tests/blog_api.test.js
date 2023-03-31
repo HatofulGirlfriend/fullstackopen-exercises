57a92f454dc6e0129724841e63dd4b03cd54c95e
Toni Patsias <enchanted.toilet@gmail.com>
2023-03-31 11:54:30 +0100

    4.9: Blog list tests, step2

--- part_4/tests/blog_api.test.js
+++ part_4/tests/blog_api.test.js
@@ -11,7 +11,11 @@ test("all blogs are returned", async () => {
   expect(response.body).toHaveLength(2)
 }, 100000)
 
-
+test("expect id to be defined", async () => {
+  const response = await api.get("/api/blogs")
+  expect(response.body[0].id).toBeDefined()
+  expect(response.body[1].id).toBeDefined()
+})
 
 afterAll(async () => {
   await mongoose.connection.close()
