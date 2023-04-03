1e020e861a987b3befbf84dc7b7ba2ebc700c319
Toni Patsias <enchanted.toilet@gmail.com>
2023-04-03 20:41:03 +0100

    4.20*: bloglist expansion, step8

--- part_4/controllers/blogs.js
+++ part_4/controllers/blogs.js
@@ -3,13 +3,13 @@ const Blog = require("../models/blog")
 const User = require("../models/user")
 const jwt = require("jsonwebtoken")
 
-const getTokenFrom = request => {
-  const authorization = request.get("authorization")
-  if (authorization && authorization.startsWith("Bearer ")) {
-    return authorization.replace("Bearer ", "")
-  }
-  return null
-}
+// const getTokenFrom = request => {
+//   const authorization = request.get("authorization")
+//   if (authorization && authorization.startsWith("Bearer ")) {
+//     return authorization.replace("Bearer ", "")
+//   }
+//   return null
+// }
 
 blogsRouter.get("/", async(request, response) => {
   const blogs = await Blog
@@ -19,8 +19,10 @@ blogsRouter.get("/", async(request, response) => {
 })
 
 blogsRouter.post("/", async (request, response) => {
+  console.log("request in blogsRouter post is", request)
   const body = request.body
-  const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET)
+  console.log("request.token is", request.token)
+  const decodedToken = jwt.verify(request.token, process.env.SECRET)
   if (!decodedToken.id) {
     return response.status(401).json({ error: "token invalid" })
   }
